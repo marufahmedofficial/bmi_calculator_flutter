50b9274ec3a4d632da3d1a22d3d4c48d114e1155
marufahmedofficial <mk6116055@gmail.com>
2023-05-06 18:47:33 +0600

    initial commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -31,97 +31,94 @@ class HomeScreenBMIState extends State<HomeScreenBMI> {
           centerTitle: true,
           title: const Text("BMI Calculator"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: SingleChildScrollView(
-            child: Container(
-              padding: const EdgeInsets.all(12),
-              child: Card(
-                elevation: 12,
-                shape: const RoundedRectangleBorder(),
-                child: Expanded(
-                  child: Column(
-                    children: [
-                      //Lets create widget for gender selection
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 30),
-                        child: GenderWidget(
-                          onChange: (genderVal) {
-                            _gender = genderVal;
-                          },
-                        ),
-                      ),
-                      HeightWidget(
-                        onChange: (heightVal) {
-                          _height = heightVal;
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(12),
+            child: Card(
+              elevation: 12,
+              shape: const RoundedRectangleBorder(),
+              child: Expanded(
+                child: Column(
+                  children: [
+                    //Lets create widget for gender selection
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 30),
+                      child: GenderWidget(
+                        onChange: (genderVal) {
+                          _gender = genderVal;
                         },
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 30),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            AgeWeightWidget(
-                                onChange: (ageVal) {
-                                  _age = ageVal;
-                                },
-                                title: "Age",
-                                initValue: 20,
-                                min: 0,
-                                max: 100),
-                            AgeWeightWidget(
-                                onChange: (weightVal) {
-                                  _weight = weightVal;
-                                },
-                                title: "Weight(Kg)",
-                                initValue: 55,
-                                min: 0,
-                                max: 200)
-                          ],
-                        ),
+                    ),
+                    HeightWidget(
+                      onChange: (heightVal) {
+                        _height = heightVal;
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          AgeWeightWidget(
+                              onChange: (ageVal) {
+                                _age = ageVal;
+                              },
+                              title: "Age",
+                              initValue: 20,
+                              min: 0,
+                              max: 100),
+                          AgeWeightWidget(
+                              onChange: (weightVal) {
+                                _weight = weightVal;
+                              },
+                              title: "Weight(Kg)",
+                              initValue: 55,
+                              min: 0,
+                              max: 200)
+                        ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 60),
-                        child: SwipeableButtonView(
-                            isFinished: _isFinished,
-                            onFinish: () async {
-                              await Navigator.push(
-                                  context,
-                                  PageTransition(
-                                      child: ScoreScreen(
-                                        bmiScore: _bmiScore,
-                                        age: _age,
-                                      ),
-                                      type: PageTransitionType.fade));
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 60),
+                      child: SwipeableButtonView(
+                          isFinished: _isFinished,
+                          onFinish: () async {
+                            await Navigator.push(
+                                context,
+                                PageTransition(
+                                    child: ScoreScreen(
+                                      bmiScore: _bmiScore,
+                                      age: _age,
+                                    ),
+                                    type: PageTransitionType.fade));
 
+                            setState(() {
+                              _isFinished = false;
+                            });
+                          },
+                          onWaitingProcess: () {
+                            //Calculate BMI here
+                            calculateBmi();
+
+                            Future.delayed(const Duration(seconds: 1), () {
                               setState(() {
-                                _isFinished = false;
+                                _isFinished = true;
                               });
-                            },
-                            onWaitingProcess: () {
-                              //Calculate BMI here
-                              calculateBmi();
-
-                              Future.delayed(const Duration(seconds: 1), () {
-                                setState(() {
-                                  _isFinished = true;
-                                });
-                              });
-                            },
-                            activeColor: Colors.blue,
-                            buttonWidget: const Icon(
-                              Icons.arrow_forward_ios_rounded,
-                              color: Colors.black,
-                            ),
-                            buttonText: "CALCULATE"),
-                      ),
-                      const Padding(  padding: EdgeInsets.symmetric(
-                          vertical: 16)),
-                    ],
-                  ),
+                            });
+                          },
+                          activeColor: Colors.blue,
+                          buttonWidget: const Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.black,
+                          ),
+                          buttonText: "CALCULATE"),
+                    ),
+                    const Padding(  padding: EdgeInsets.symmetric(
+                        vertical: 16)),
+                  ],
                 ),
               ),
             ),
